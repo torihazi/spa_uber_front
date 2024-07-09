dc6fdd9ba4dc1d03648d5aef3288007cfdf0eee0
torihazi <lovebarusa19981225@icloud.com>
2024-07-09 13:25:57 +0900

    fin chapter7-1

--- src/containers/Orders.jsx
+++ src/containers/Orders.jsx
@@ -1,6 +1,29 @@
-import React from "react";
+import React, { useEffect, useReducer } from "react";
+
+import { fetchLineFoods } from "../apis/line_foods";
+
+import {
+  initialState,
+  lineFoodsActionTypes,
+  lineFoodsReducer,
+} from "../reducers/lineFoods";
 
 const Orders = () => {
+  const [state, dispatch] = useReducer(lineFoodsReducer, initialState);
+  useEffect(() => {
+    dispatch({ type: lineFoodsActionTypes.FETCHING });
+    fetchLineFoods()
+      .then((data) => {
+        dispatch({
+          type: lineFoodsActionTypes.FETCH_SUCCESS,
+          payload: {
+            lineFoodsSummary: data,
+          },
+        });
+      })
+      .catch((e) => console.error(e));
+  }, []);
+
   return <div>注文画面</div>;
 };
 
